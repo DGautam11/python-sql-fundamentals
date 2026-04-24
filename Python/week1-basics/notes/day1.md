3a1d69eaa3c549c7837dbd32b19c569cb5ab14d8
Deepan Gautam <dpngtm@gmail.com>
2026-04-24 13:07:43 +0300

    docs: day 1 revision notes and practice files - Python fundamentals refresher

---
+++ Python/week1-basics/notes/day1.md
@@ -0,0 +1,3 @@
+Revised different data types,conditionals, loops, functions, importing modules,some standard libary. From Corey Schafer python playlist.
+Read chapter 2- strings, numbers, comments from python crash course book and did the try it yourself exercises
+
---
+++ Python/week1-basics/numbers.py
@@ -0,0 +1,9 @@
+#Constants
+# Constants are variables that are not meant to be changed. By convention, they are usually written in all uppercase letters with underscores separating words.
+
+PI = 3.14159
+SPEED_OF_LIGHT = 299792458
+
+print(((1*5) / 5) -1 + 8)
+favorite_number = 42
+print(f'favourite_number: {favorite_number}')
